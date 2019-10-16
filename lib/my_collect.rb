f0eba18194b92cc902d00e1194bbcefc8b6c8ddf
Deanna Kelley <deanna.georges@gmail.com>
2019-10-16 18:34:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,18 +1,14 @@
-def my_collect
-
+def my_collect(collection)
+  i = 0 
+  array = []
+  while i < collection.length 
+    array << yield(collection[i])
+    i += 1 
+  end
+  array 
 end 
 
-require 'pry'
 
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
 
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
